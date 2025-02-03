a92539b3e9115ce900e073a11489e2f791433a96
Lazar Milikic <lazar.milikic@epfl.ch>
2025-02-03 13:14:09 +0100

    Amazon reviews data

--- yelp-multilang.sh
+++
@@ -1,3 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-
-python src/trainer.py --model bert-base-multilingual-uncased --dataset yelp --n_clusters 100 --lr 5e-4 --cluster_weight 0.1 --seed 42 --do_cluster --do_inference
